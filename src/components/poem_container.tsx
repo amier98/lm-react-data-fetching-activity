aa37ef5039859f9e02692e90567ad8000d8b1ec3
Amier Ali <amier_1998@hotmail.com>
2023-12-07 20:09:24 +0000

    feat: Implement mapping for poems and add loading state

--- src/components/poem_container.tsx
+++ src/components/poem_container.tsx
@@ -1,30 +1,36 @@
-import { useEffect, useState } from "react"
-import { PoemItem } from "../mock_api/data"
-import { PoemsList } from "./poems_list"
-import { AddPoem } from "./add_poem"
+import { useEffect, useState } from "react";
+import { PoemItem } from "../mock_api/data";
+import { PoemsList } from "./poems_list";
+import { AddPoem } from "./add_poem";
+import { ShowLoading } from "./show_loading";
 
 export interface PoemData extends PoemItem {
-  id: number
+  id: number;
 }
-export type PoemsResponse = Array<PoemData>
+export type PoemsResponse = Array<PoemData>;
 
 export const PoemContainer: React.FC = () => {
-  const [poems, setPoems] = useState<PoemsResponse>([])
+  const [poems, setPoems] = useState<PoemsResponse>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getPoems() {
-      const data = await fetch("/poetriumph.com/api/v1/poems")
-      const result: PoemsResponse = await data.json()
-      console.log(result)
+      if (loading) return;
+      setLoading(true);
+      const data = await fetch("/poetriumph.com/api/v1/poems");
+      const result: PoemsResponse = await data.json();
+      setPoems(result);
+      setLoading(false);
     }
-    getPoems()
-  }, [])
+    getPoems();
+  }, []);
 
   return (
     <>
       <h1>Nature Poems: An Articifial Perspective</h1>
-      <AddPoem setPoems={setPoems} />
-      <PoemsList poems={poems} />
+      {loading && <ShowLoading />}
+      {!loading && <AddPoem setPoems={setPoems} />}
+      {!loading && <PoemsList poems={poems} />}
     </>
-  )
-}
+  );
+};
